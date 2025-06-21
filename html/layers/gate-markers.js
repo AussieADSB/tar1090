4f352ec7e53006d925846dcef2776b7d27e0fcbd
Dominic Hubert <djdom2364@gmail.com>
2025-06-21 19:30:39 +1000

    Didn't commit the compiled js again

---
+++ html/layers/gate-markers.js
@@ -0,0 +1,51 @@
+"use strict";
+class GateMarkersLayer {
+    addLayers() {
+        $.getJSON("/api/data/gates", (data) => {
+            const source = new ol.source.Vector({
+                features: data.map((item, i) => new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([item[1][1], item[1][0]])),
+                    text: item[0],
+                    zIndex: i
+                }))
+            });
+            let airportsGroup = layers.getArray().find(layer => layer.get('name') === 'airports');
+            airportsGroup.getLayers().push(new ol.layer.Vector({
+                source: source,
+                // @ts-ignore
+                name: 'gate_markers',
+                title: 'Gate Markers',
+                type: 'overlay',
+                visible: true,
+                minZoom: 15,
+                zIndex: 98,
+                style: function (feature) {
+                    return [new ol.style.Style({
+                            zIndex: feature.get('zIndex'),
+                            image: new ol.style.Circle({
+                                radius: 14,
+                                fill: new ol.style.Fill({
+                                    color: 'black'
+                                }),
+                                stroke: new ol.style.Stroke({
+                                    color: '#f5a600',
+                                    width: 2
+                                })
+                            })
+                        }),
+                        new ol.style.Style({
+                            zIndex: feature.get('zIndex'),
+                            text: new ol.style.Text({
+                                text: feature.get('text'),
+                                font: `bold ${feature.get('text').length === 4 ? '10px' : '12px'} monospace`,
+                                fill: new ol.style.Fill({
+                                    color: '#f5a600'
+                                })
+                            })
+                        })];
+                }
+            }));
+        });
+    }
+}
+window.gateMarkersLayer = new GateMarkersLayer();
